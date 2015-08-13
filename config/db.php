15cbcbb1c6ed017af00c5126672d3a7da5a80d36
Kevin Dantas <kevin.ds.29@gmail.com>
2015-08-13 11:32:09 -0300

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=destino_certo',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
