a07a34bbab0962edfc59e4c9c9bbdec5fd70fe36
Kevin-ds <kevin.ds.29@gmail.com>
2015-08-15 12:08:43 -0300

    Fix isCooperative view

--- frontend/views/site/view.php
+++ frontend/views/site/view.php
@@ -33,7 +33,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'lng',
             'name',
             'taxId',
-            'isCooperative',
+            'site_type',
         ],
     ]) ?>
 
